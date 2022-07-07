0edf88bac3dbaf6df844a256b4282e45571b5949
valerioigor <igorvalerio@yahoo.com>
2022-07-07 19:51:42 -0300

    plot map & wind

--- application/blueprints/routes.py
+++ application/blueprints/routes.py
@@ -1,4 +1,3 @@
-from turtle import title
 from flask import render_template, flash, redirect, url_for, request, session
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.urls import url_parse
@@ -23,47 +22,120 @@ def init_app(app):
 
     @app.route('/<buoy>')
     @login_required
-    def generic(buoy=None):
-
-        outrafig = go.Figure(go.Scattermapbox(
-            mode = "markers+lines",
-            lon = [10, 20, 30],
-            lat = [10, 20,30],
-            marker = {'size': 10}))
-
-        outrafig.add_trace(go.Scattermapbox(
-            mode = "markers+lines",
-            lon = [-50, -60,40],
-            lat = [30, 10, -20],
-            marker = {'size': 10}))
-
-        outrafig.update_layout(
-            margin ={'l':0,'t':0,'b':0,'r':0},
-            mapbox = {
-                'center': {'lon': 10, 'lat': 10},
-                'style': "stamen-terrain",
-                'center': {'lon': -20, 'lat': -20},
-                'zoom': 1})
-
-        outrafig = outrafig.to_html(full_html=False)
-
-        return render_template(
-            'buoy.html', 
-            buoy=buoy,
-            div_placeholder=outrafig,
-
-        )
+    def generic(buoy):
 
 
-    @app.route('/<buoy>/<type>')
-    @login_required
-    def generic2(buoy=None, type=None):
-        
-        
-        return render_template(
-            'buoy2.html', 
-            buoy=buoy,
-        )
+        BUOYS = [
+            'noronha',
+            'alcatrazes',
+            'mexilhao',
+            'bc1',
+            'keller',
+            'pinguim',
+            '_potter',
+            'abrolhos',
+            'trindade',
+            'bmo_santos',
+            'antartica',
+            'minuano',
+            'itaguai',
+            'itaoca',
+            'niteroi2',
+            'fortaleza',
+            'vitoria',
+            'cabofrio2',
+            'niteroi',
+            'santos',
+            'itajai',
+            'riogrande',
+            'recife',
+            'portoseguro',
+            'cabofrio',
+        ]
+
+        if buoy in BUOYS:
+            df = pd.read_sql(buoy, db.engine)
+###############################################################################################################
+            import plotly.graph_objects as go
+
+            fig = go.Figure(go.Scattermapbox(
+                mode = "markers+lines",
+                lon = df['lon'],
+                lat = df['lat'],
+                marker = {
+                    'color':'red',
+                    'size': 5},
+            ))
+
+
+            fig.update_layout(
+                margin ={'l':250,'t':0,'b':0,'r':250},
+                mapbox = {
+                    'style': "stamen-terrain",
+                    'center': {'lon': 0, 'lat': 0},
+                    'zoom': 0.8})
+                    
+            fig = fig.to_html()
+
+##################################################################################################################
+
+            import numpy as np
+
+            wind = go.Figure(go.Scatter(x=df['Datetime'], y=df['wspd']))
+            if 'gust' in list(df.columns):
+                wind.add_trace(go.Scatter(x=df.index, y=df['gust']))
+            
+            wind2 = wind.to_html()
+            wave = wind.to_html() ##################################################
+
+            windrose = go.Figure()
+
+            windrose.add_trace(go.Barpolar(
+                r=[77.5, 72.5, 70.0, 45.0, 22.5, 42.5, 40.0, 62.5],
+                name='11-14 m/s',
+                marker_color='rgb(106,81,163)'
+            ))
+            windrose.add_trace(go.Barpolar(
+                r=[57.5, 50.0, 45.0, 35.0, 20.0, 22.5, 37.5, 55.0],
+                name='8-11 m/s',
+                marker_color='rgb(158,154,200)'
+            ))
+            windrose.add_trace(go.Barpolar(
+                r=[40.0, 30.0, 30.0, 35.0, 7.5, 7.5, 32.5, 40.0],
+                name='5-8 m/s',
+                marker_color='rgb(203,201,226)'
+            ))
+            windrose.add_trace(go.Barpolar(
+                r=[20.0, 7.5, 15.0, 22.5, 2.5, 2.5, 12.5, 22.5],
+                name='< 5 m/s',
+                marker_color='rgb(242,240,247)'
+            ))
+
+            windrose.update_traces(text=['North', 'N-E', 'East', 'S-E', 'South', 'S-W', 'West', 'N-W'])
+            windrose.update_layout(
+                title='Wind Speed Distribution in Laurel, NE',
+                font_size=16,
+                legend_font_size=16,
+                polar_radialaxis_ticksuffix='%',
+                polar_angularaxis_rotation=90,
+
+            )
+
+            windrose2 = windrose.to_html()
+            waverose = windrose.to_html() ##########################################################
+
+
+
+            return render_template(
+                'buoy.html', 
+                buoy=buoy,
+                div_map=fig,
+                div_wind=wind2,
+                div_windrose=windrose2,
+                div_wave = wave,
+                div_waverose = waverose,
+            )
+        return render_template('home.html')
 
 
     @app.route('/buoy-table')
--- scrapping/scrapping.py
+++ scrapping/scrapping.py
@@ -47,35 +47,44 @@ for key, value in data_urls_dict.items():
         df_temp = df_temp.rename(columns={'# Datetime':'Datetime'})
         df_temp = df_temp.set_index('Datetime').sort_index(ascending=False)
 
+
+    df_temp.columns = [x.lower() for x in list(df_temp.columns)]
+
+    if list(df_temp.columns) == ['condutividade', 'direcao_vento', 'latitude','longitude','pressao_atm', 'rad_solar', 'salinidade', 'temp_agua', 'temp_ar','umidade', 'velocidade_vento']:
+        df_temp.columns = ['cond','wdir','lat','lon','atmp','rad','salinidade','wtemp','temp', 'hum','wspd']
+    # print(len(df_temp.columns))
+    # print(df_temp.columns)
     # create df_status
-    last_aquisition = df_temp.iloc[[0]].index[0]
-    date_of_last_aquisition = datetime.strptime(last_aquisition, '%Y-%m-%d %H:%M:%S').date()
-    today = date.today()
 
-    if (today - date_of_last_aquisition).days <= 7:
-        status = 'Operacional'
-    else: status = 'Manutenção'
+    print(df_temp.columns)
+#     last_aquisition = df_temp.iloc[[0]].index[0]
+#     date_of_last_aquisition = datetime.strptime(last_aquisition, '%Y-%m-%d %H:%M:%S').date()
+#     today = date.today()
+
+#     if (today - date_of_last_aquisition).days <= 7:
+#         status = 'Operacional'
+#     else: status = 'Manutenção'
     
-    dic= {'buoy':[key], 'status':[status], 'last_aquisition':[last_aquisition]}
+#     dic= {'buoy':[key], 'status':[status], 'last_aquisition':[last_aquisition]}
 
-    df_fromDic = pd.DataFrame.from_dict(dic)
-    df_status = pd.concat([df_status, df_fromDic], ignore_index=True)
+#     df_fromDic = pd.DataFrame.from_dict(dic)
+#     df_status = pd.concat([df_status, df_fromDic], ignore_index=True)
 
-    df_last = pd.read_sql(f'SELECT * FROM {key};',con=conn)
-    df_last = df_last.set_index('Datetime')
-    df_temp = pd.concat([df_last, df_temp], ignore_index=False)
+#     df_last = pd.read_sql(f'SELECT * FROM {key};',con=conn)
+#     df_last = df_last.set_index('Datetime')
+#     df_temp = pd.concat([df_last, df_temp], ignore_index=False)
     
-    df_temp = df_temp.drop_duplicates()
-    df_temp = df_temp.sort_index(ascending=False)
+#     df_temp = df_temp.drop_duplicates()
+#     df_temp = df_temp.sort_index(ascending=False)
     
-    #limitar em 1 ano de dado
-    # df_temp.index = pd.to_datetime(df_temp.index)
-    # df_temp = df_temp[df_temp.index >= df_temp.index[0]- timedelta(days=365)]
+#     #limitar em 1 ano de dado
+#     # df_temp.index = pd.to_datetime(df_temp.index)
+#     # df_temp = df_temp[df_temp.index >= df_temp.index[0]- timedelta(days=365)]
+    
+#     df_temp.to_sql(name=key.title(), con=conn, if_exists='replace')
+#     print(key, 'ok')
 
-    df_temp.to_sql(name=key, con=conn, if_exists='replace')
-    print(key, 'ok')
 
-
-df_status = df_status.sort_values('last_aquisition', ascending=False, ignore_index=True)
-df_status.to_sql(name='buoyStatus', con=conn, if_exists='replace')
-print('dfstatus ok')
+# df_status = df_status.sort_values('last_aquisition', ascending=False, ignore_index=True)
+# df_status.to_sql(name='buoyStatus', con=conn, if_exists='replace')
+# print('dfstatus ok')
