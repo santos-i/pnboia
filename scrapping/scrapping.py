ea3d4585c580fc352ea54f68259b8ccc856852ef
Igor Santos <santoigor@icloud.com>
2022-07-12 16:29:51 -0300

    normalização de colunas dos dfs

--- scrapping/scrapping.py
+++ scrapping/scrapping.py
@@ -31,6 +31,7 @@ for buoy in BUOYS:
 
 df_status = pd.DataFrame()
 
+
 for key, value in data_urls_dict.items():
     dataSoup = return_soup(value)
     data = dataSoup.get_text()
@@ -50,41 +51,72 @@ for key, value in data_urls_dict.items():
 
     df_temp.columns = [x.lower() for x in list(df_temp.columns)]
 
-    if list(df_temp.columns) == ['condutividade', 'direcao_vento', 'latitude','longitude','pressao_atm', 'rad_solar', 'salinidade', 'temp_agua', 'temp_ar','umidade', 'velocidade_vento']:
-        df_temp.columns = ['cond','wdir','lat','lon','atmp','rad','salinidade','wtemp','temp', 'hum','wspd']
-    # print(len(df_temp.columns))
-    # print(df_temp.columns)
-    # create df_status
+    """
+    12: ['lat', 'lon', 'wspd', 'wdir', 'wtmp', 'wvht', 'dpd', 'mwd', 'spred', 'peak_mwd', 'peak_spred', 'mean_dpd'], 
+    11: ['condutividade', 'direcao_vento', 'latitude', 'longitude', 'pressao_atm', 'rad_solar', 'salinidade', 'temp_agua', 'temp_ar', 'umidade', 'velocidade_vento'], 
+    27: ['lat', 'lon', 'battery', 'compass', 'wspd', 'gust', 'wdir', 'atmp', 'dewpt', 'rh', 'pres', 'arad', 'sst', 'cspd1', 'cdir1', 'cspd2', 'cdir2', 'cspd3', 'cdir3', 'swvht1', 'tp1', 'mxwvht1', 'wvdir1', 'wvspread1', 'swvht2', 'tp2', 'wvdir2'], 
+    23: ['lat', 'lon', 'battery', 'bhead', 'wspd', 'wdir', 'gust', 'atmp', 'pres', 'dewp', 'humi', 'wtmp', 'cvel1', 'cdir1', 'cvel2', 'cdir2', 'cvel3', 'cdir3', 'wvht', 'wmax', 'dpd', 'mwd', 'spread']}
+    """
 
-    print(df_temp.columns)
-#     last_aquisition = df_temp.iloc[[0]].index[0]
-#     date_of_last_aquisition = datetime.strptime(last_aquisition, '%Y-%m-%d %H:%M:%S').date()
-#     today = date.today()
-
-#     if (today - date_of_last_aquisition).days <= 7:
-#         status = 'Operacional'
-#     else: status = 'Manutenção'
+    # 1 boia
+    df_11_cols = ['condutividade', 'wdir', 'lat', 'lon','pres', 'rad_solar', 'sal', 'wtmp', 'atmp','humi', 'wdir']
+    # 8 boias
+    df_12_cols = ['lat', 'lon', 'wspd', 'wdir', 'wvtmp', 'wvht', 'dpd', 'wvdir', 'spred','peak_mwd', 'peak_spred', 'mean_dpd']
+    # 13 boias
+    df_23_cols = ['lat', 'lon', 'battery', 'bhead', 'wspd', 'wdir', 'gust', 'atmp','pres', 'dewp', 'humi', 'wvtmp', 'cvel1', 'cdir1', 'cvel2', 'cdir2','cvel3', 'cdir3', 'wvht', 'mxwvht', 'dpd', 'wvdir', 'spread']
+    # 3 boias
+    df_27_cols = ['lat', 'lon', 'battery', 'compass', 'wspd', 'gust', 'wdir', 'atmp','dewpt', 'rh', 'pres', 'arad', 'wvtmp', 'cspd1', 'cdir1', 'cspd2','cdir2', 'cspd3', 'cdir3', 'wvht', 'dpd', 'mxwvht1', 'wvdir','wvspread', 'swvht2', 'tp2', 'wvdir2']
     
-#     dic= {'buoy':[key], 'status':[status], 'last_aquisition':[last_aquisition]}
-
-#     df_fromDic = pd.DataFrame.from_dict(dic)
-#     df_status = pd.concat([df_status, df_fromDic], ignore_index=True)
-
-#     df_last = pd.read_sql(f'SELECT * FROM {key};',con=conn)
-#     df_last = df_last.set_index('Datetime')
-#     df_temp = pd.concat([df_last, df_temp], ignore_index=False)
+    df_cols_new = ['lat', 'lon', 'wspd', 'wdir', 'wvtmp', 'wvht', 'wvdir', 'dpd']
     
-#     df_temp = df_temp.drop_duplicates()
-#     df_temp = df_temp.sort_index(ascending=False)
+    if len(df_temp.columns) != 11: 
+
     
-#     #limitar em 1 ano de dado
-#     # df_temp.index = pd.to_datetime(df_temp.index)
-#     # df_temp = df_temp[df_temp.index >= df_temp.index[0]- timedelta(days=365)]
-    
-#     df_temp.to_sql(name=key.title(), con=conn, if_exists='replace')
-#     print(key, 'ok')
+        if len(df_temp.columns) == 12:
+            df_temp.columns = df_12_cols
+            df_temp = df_temp[df_cols_new]
+        if len(df_temp.columns) == 23:
+            df_temp.columns = df_23_cols
+            df_temp = df_temp[df_cols_new]
+        if len(df_temp.columns) == 27:
+            df_temp.columns = df_27_cols
+            df_temp = df_temp[df_cols_new]
 
 
-# df_status = df_status.sort_values('last_aquisition', ascending=False, ignore_index=True)
-# df_status.to_sql(name='buoyStatus', con=conn, if_exists='replace')
-# print('dfstatus ok')
+        # create df_status
+        last_aquisition = df_temp.iloc[[0]].index[0]
+        date_of_last_aquisition = datetime.strptime(last_aquisition, '%Y-%m-%d %H:%M:%S').date()
+        today = date.today()
+
+        if (today - date_of_last_aquisition).days <= 7:
+            status = 'Operacional'
+        else: status = 'Manutenção'
+        
+        if key == '_potter':
+            key = 'potter'
+
+        dic= {'buoy':[key], 'status':[status], 'last_aquisition':[last_aquisition]}
+
+        df_fromDic = pd.DataFrame.from_dict(dic)
+        df_status = pd.concat([df_status, df_fromDic], ignore_index=True)
+        try:
+            df_last = pd.read_sql(f'SELECT * FROM {key};',con=conn)
+            df_last = df_last.set_index('Datetime')
+            df_temp = pd.concat([df_last, df_temp], ignore_index=False)
+        except:
+            pass
+
+        df_temp = df_temp.drop_duplicates()
+        df_temp = df_temp.sort_index(ascending=False)
+        
+        #limitar em 1 ano de dado
+        # df_temp.index = pd.to_datetime(df_temp.index)
+        # df_temp = df_temp[df_temp.index >= df_temp.index[0]- timedelta(days=365)]
+        
+        df_temp.to_sql(name=key, con=conn, if_exists='replace')
+        print(key, 'ok')
+
+
+df_status = df_status.sort_values('last_aquisition', ascending=False, ignore_index=True)
+df_status.to_sql(name='buoyStatus', con=conn, if_exists='replace')
+print('dfstatus ok')
